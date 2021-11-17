95d9e62cce86e14972fa9f8c1cf5d7db85797261
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-11-17 21:05:11 +0530

    a

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+const users = require("./MOCK_DATA.json");
+
+// app.get('/',(req,res) => {
+//     const b = "Welcome to homepage";
+//      res.send(b);
+// })
+
+app.get('/',(req,res) => {
+return res.send(users);
+})
+
+app.listen(8000, function (){
+
+    console.log('listening on port 8000')
+})
+
+
+
+
+
